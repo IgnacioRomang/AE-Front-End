7dd7a929b20d86bb6637fd73a4b8c73e72e4667d
Ignacio M. Romang <ignacioromang@outlook.com>
2023-11-23 16:11:21 -0300

    sdfas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Profiler, useState } from "react";
+import { createRoot } from "react-dom/client";
+
+import Footer from "./components/Footer.jsx";
+import LoginCard from "./components/LoginCard.jsx";
+import TopBar from "./components/TopBar.jsx";
+import RegisterCard from "./components/RegisterCard.jsx";
+import ScrollableComponent from "./fragments/ScrollableComponent.jsx";
+import InfoCard from "./components/InfoCard.jsx";
+import { Stack } from "@mui/system";
+import RenewalCard from "./components/RenewalCard.jsx";
+import UserBadge from "./fragments/UserBadge.jsx";
 
 function App() {
+  const [userAuth, setUserAuth] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+                  <TopBar userAuth={userAuth} />
+            <ScrollableComponent
+                styles={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexGrow: 1,
+                }}
+            >
+                <Stack spacing={2}>
+                    <UserBadge username="Ignacio Romang" isActive={false} />
+                    <RenewalCard />
+                    <RegisterCard />
+                    <LoginCard />
+                    <InfoCard />
+                </Stack>
+                <Footer></Footer>
+            </ScrollableComponent>
     </div>
   );
 }
